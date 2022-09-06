7ee2bfec52cc46ce5639d923527c61e47e213cdb
Bangar Raju T <bangaraju.59@gmail.com>
2022-09-06 17:58:21 +0530

    Terraform | terraform.auto.tfvars

--- 02-vars/vars.tf
+++ 02-vars/vars.tf
@@ -1,15 +1,16 @@
 variable "test" {
  default = "hello"
 }
-
 output "test" {
  value=var.test
 }
 
-variable "test1" {
-
-}
-
+variable "test1" {}
 output "test1" {
  value=var.test1
+}
+
+variable "test2" {}
+output "test2" {
+ value=var.test2
 }
